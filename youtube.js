00923963886126ca906e687a7763d7798bdd6b0b
Ted Wu <tedwu2016@u.northwestern.edu>
2016-04-26 13:34:25 -0500

    Youtube iframe api initializations

--- youtube.js
+++ youtube.js
@@ -1,1 +1,34 @@
-youtube.js
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-placeholder', {
+        width: 600,
+        height: 400,
+        videoId: 'Xa0Q0J5tOP0',
+        playerVars: {
+            color: 'white',
+            playlist: 'taJ60kskkns,FG0fTKAqZ5g'
+        },
+        events: {
+            onReady: initialize
+        }
+    });
+}
+
+function initialize() {
+    // Update the controls on load
+        updateTimerDisplay();
+        updateProgressBar();
+
+        // Clear any old interval.
+        clearInterval(time_update_interval);
+
+        // Start interval to update elapsed time display and
+        // the elapsed part of the progress bar every second.
+        time_update_interval = setInterval(function () {
+            updateTimerDisplay();
+            updateProgressBar();
+        }, 1000)
+
+    
+}
